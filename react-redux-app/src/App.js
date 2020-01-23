d2bbfeb7a38e47ed53f172c131bdc0c1fad48f6f
arashhaji <56742302+arashhaji@users.noreply.github.com>
2020-01-23 18:13:41 -0500

    mvp

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import PlayerCard from './components/PlayerCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <div className="bg">
+        <h1>NBA PLAYER CARDS</h1>
+        <a href="#player">View Players</a>
+      </div>
+      <div id="player">
+        <PlayerCard />
+      </div>
+
     </div>
   );
 }
